82d43f71b592b6a49e1862fc8797b51b3430f359
Douglass Clem <crashsystems@gmail.com>
2013-09-28 16:05:37 -0700

    All data is now moved into /srv/gitlab/data
    This should make it possible for users to update GitLab by
    rebuilding/pulling the image, replaceing the second half of firstrun.sh
    with their DB migrations, and doing a docker run with the -v param
    mounting the external folder containing the data to /srv/gitlab
    internally.

--- firstrun.sh
+++ firstrun.sh
@@ -3,7 +3,7 @@
 # Set these parameters
 mysqlRoot=RootPassword
 
-# ==== Do not edit below this line ===
+# === Do not modify anything in this section ===
 
 # Copy over config files
 mv /srv/gitlab/config/nginx /etc/nginx/sites-available/gitlab
@@ -11,14 +11,27 @@ ln -s /etc/nginx/sites-available/gitlab /etc/nginx/sites-enabled/gitlab
 mv /srv/gitlab/config/gitlab.yml /home/git/gitlab/config/gitlab.yml
 
 password=$(cat /srv/gitlab/config/database.yml | grep -m 1 password | sed -e 's/  password: "//g' | sed -e 's/"//g')
-sed -i -e "s/root/gitlab/g" /srv/gitlab/config/database.yml
-sed -i -e "s/secure password/$password/g" /srv/gitlab/config/database.yml
 mv /srv/gitlab/config/database.yml /home/git/gitlab/config/database.yml
 chown git:git /home/git/gitlab/config/database.yml && chmod o-rwx /home/git/gitlab/config/database.yml
 
-# Move mysql data dir to /srv/gitlab
-mv /var/lib/mysql /srv/gitlab
-ln -s /srv/gitlab/mysql /var/lib/mysql
+# Link data directories to /srv/gitlab/data
+rm -R /home/git/gitlab/tmp && ln -s /srv/gitlab/data/tmp /home/git/gitlab/tmp && chown -R git /srv/gitlab/data/tmp/ && chmod -R u+rwX  /srv/gitlab/data/tmp/
+rm -R /home/git/.ssh && ln -s /srv/gitlab/data/ssh /home/git/.ssh && chown -R git:git /srv/gitlab/data/ssh && chmod -R 0777 /srv/gitlab/data/ssh
+rm -R /home/git/gitlab-satellites && ln -s /srv/gitlab/data/gitlab-satellites /home/git/gitlab-satellites && chown -R git:git /srv/gitlab/data/gitlab-satellites
+rm -R /home/git/repositories && ln -s /srv/gitlab/data/repositories /home/git/repositories && chown -R git:git /srv/gitlab/data/repositories && chmod -R ug+rwX,o-rwx /srv/gitlab/data/repositories
+
+# Link MySQL dir to /srv/gitlab/data
+mv /var/lib/mysql /var/lib/mysql-tmp
+ln -s /srv/gitlab/data/mysql /var/lib/mysql
+
+# ==============================================
+
+# === Delete this section if resoring data from previous build ===
+
+rm -R /srv/gitlab/data/mysql
+mv /var/lib/mysql-tmp /srv/gitlab/data/mysql
+
+# Start MySQL
 mysqld_safe &
 sleep 5
 
@@ -36,6 +49,8 @@ cd /home/git/gitlab
 su git -c "bundle exec rake gitlab:setup force=yes RAILS_ENV=production"
 sleep 5
 su git -c "bundle exec rake db:seed_fu RAILS_ENV=production"
+
+# ================================================================
 
 # Manually create /var/run/sshd
 mkdir /var/run/sshd
